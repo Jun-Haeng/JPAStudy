6ed8e7023db5234b585bcc291284f89abe41f029
JunHaeng3 <gjwnsgod123@hanmail.com>
2023-02-17 19:04:16 +0900

    0217 study

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+                "hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZ");
+
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
